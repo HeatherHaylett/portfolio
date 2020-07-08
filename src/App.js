8304d681b2e664fb620a3587c721d62426190f93
Heather Haylett <heatherhaylett@gmail.com>
2020-07-07 21:43:50 -0500

    intall react router and starting on navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import './App.scss';
-import Image from 'react-bootstrap/Image';
-import mainImage from './Haylett.jpg';
+//import Image from 'react-bootstrap/Image';
+//import mainImage from './Haylett.jpg';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Heather's portfolio</h1>
-        <Image src={mainImage} rounded />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/myWork">My Work</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/myWork">
+            <MyWork />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function MyWork() {
+  return <h2>My Work</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+
