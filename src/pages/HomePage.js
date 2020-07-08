01895afe6370a12173be5879192b41297a00f214
Heather Haylett <heatherhaylett@gmail.com>
2020-07-08 17:49:01 -0500

    Hero component and image component side by side in one row

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Hero from '../components/Hero.js';
+import MainImage from '../components/MainImage.js';
+
 
 function HomePage(props) {
   return (
-    <Hero />
+    <Container>
+      <Row>
+        <Col>
+          <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
+        </Col>
+        <Col><MainImage /></Col>
+      </Row>
+    </Container>
   )
 }
 
