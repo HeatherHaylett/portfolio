b0c56aa01d5d306d06ccecf47df88b013f06f361
Heather Haylett <heatherhaylett@gmail.com>
2020-07-08 17:50:06 -0500

    removed jumbotron

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-import Jumbotron from 'react-bootstrap/Jumbotron'
+//import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Hero(props) {
   return (
-    <Jumbotron></Jumbotron>
+    <Row>
+      <Container d-flex>
+          <Col md={6}>
+            {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
+            { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
+            { props.text && <h3 className="lead font-weight-light">{props.text}</h3> }
+          </Col>
+      </Container>
+    </Row>
   )
 }
 
