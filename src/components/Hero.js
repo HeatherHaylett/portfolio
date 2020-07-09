5b8e25c0be1c0da3b56b7992d8673b13eaab7364
Heather Haylett <heatherhaylett@gmail.com>
2020-07-09 15:56:33 -0500

    moved buttons inside hero component and centered image and text on page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 //import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Pdf from '../assets/Resume-Portfolio.pdf';
 
 function Hero(props) {
   return (
@@ -12,6 +14,8 @@ function Hero(props) {
             {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
             { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
             { props.text && <h3 className="lead font-weight-light">{props.text}</h3> }
+            <Button variant="dark" href="/work" className='mt-2'>My Work</Button>
+          <Button variant="dark" href = {Pdf} target = "_blank" rel="noopener noreferrer" className='ml-2 mt-2'>Resume</Button>
       </Container>
     </Row>
   )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,23 +3,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+
 
 import Hero from '../components/Hero.js';
 import MainImage from '../components/MainImage.js';
-import Pdf from '../assets/Resume-Portfolio.pdf';
+
 
 
 function HomePage(props) {
   return (
     <Container>
       <Row>
-        <Col>
+        <Col className="d-flex justify-content-center">
           <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
-          <Button variant="dark" href="/work" className='mt-2'>My Work</Button>
-          <Button variant="dark" href = {Pdf} target = "_blank" rel="noopener noreferrer" className='ml-2 mt-2'>Resume</Button>
         </Col>
-        <Col><MainImage /></Col>
+        <Col className="d-flex justify-content-center"><MainImage /></Col>
       </Row>
     </Container>
   )
