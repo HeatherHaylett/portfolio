f334f32c174e4e6e0553413818e885c602ee1ca7
Heather Haylett <heatherhaylett@gmail.com>
2020-07-09 18:16:09 -0500

    add social icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import SocialFollow from './SocialFollow.js';
+
 function footer() {
   return (
     <footer className='mt-5'>
@@ -10,6 +12,9 @@ function footer() {
         <Row className="border-top justify-content-between p-3">
           <Col className='p-0' md={3} sm={12}>
             Heather Haylett
+          </Col>
+          <Col>
+            <SocialFollow />
           </Col>
           <Col className="text-right p-0" md={3}>
             Site built from scratch by Heather
