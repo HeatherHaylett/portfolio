d690086c6657e64611eaf012312793e75023a57b
Heather Haylett <heatherhaylett@gmail.com>
2020-07-08 19:24:55 -0500

    change button position

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 //import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Col from 'react-bootstrap/Col';
 
 function Hero(props) {
   return (
@@ -11,7 +11,7 @@ function Hero(props) {
       <Container>
             {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
             { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
-            { props.text && <h3 className="lead font-weight-light">{props.text}</h3> }          
+            { props.text && <h3 className="lead font-weight-light">{props.text}</h3> }
       </Container>
     </Row>
   )
