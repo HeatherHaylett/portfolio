7c375008653c4b8e32127a9f2cbb3e342057652b
Heather Haylett <heatherhaylett@gmail.com>
2020-07-09 15:01:46 -0500

    style button like homepage

--- src/pages/PDFViewer.js
+++ src/pages/PDFViewer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Pdf from '../assets/Resume-Portfolio.pdf';
 
+import Button from 'react-bootstrap/Button';
+
 class PDFViewer extends Component {
 
   render() {
 
     return (
       <div className = "App">
-        <a href = {Pdf} target = "_blank" rel="noopener noreferrer">Download Pdf</a>
+        <Button href = {Pdf}
+            target = "_blank"
+            rel="noopener noreferrer"
+            className="mt-4"
+            variant="dark"
+            >
+          Download PDF
+        </Button>
       </div>
     );
   }
