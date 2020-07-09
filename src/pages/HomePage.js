fcca8d2cdbc6a68d7892bcba25702cade34f3507
Heather Haylett <heatherhaylett@gmail.com>
2020-07-09 14:45:42 -0500

    changed resume button to pull up pdf

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button';
 
 import Hero from '../components/Hero.js';
 import MainImage from '../components/MainImage.js';
+import Pdf from '../assets/Resume-Portfolio.pdf';
 
 
 function HomePage(props) {
@@ -16,7 +17,7 @@ function HomePage(props) {
         <Col>
           <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
           <Button variant="dark" href="/work" className='mt-2'>My Work</Button>
-          <Button variant="dark" href="/resume" className='ml-2 mt-2'>Resume</Button>
+          <Button variant="dark" href = {Pdf} target = "_blank" rel="noopener noreferrer" className='ml-2 mt-2'>Resume</Button>
         </Col>
         <Col><MainImage /></Col>
       </Row>
