dae27b8730ff2ffa15c68842b9f4c7ea4cd30002
Heather Haylett <heatherhaylett@gmail.com>
2020-07-09 15:50:07 -0500

    add jpg of resume

--- src/pages/PDFViewer.js
+++ src/pages/PDFViewer.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import Pdf from '../assets/Resume-Portfolio.pdf';
 
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import ResumeImage from '../components/ResumeImage';
 
 class PDFViewer extends Component {
 
   render() {
 
     return (
-      <div className = "App">
+      <Container>
+        <Col >
+          <Row className="d-flex justify-content-center">
         <Button href = {Pdf}
             target = "_blank"
             rel="noopener noreferrer"
@@ -17,8 +24,17 @@ class PDFViewer extends Component {
             >
           Download PDF
         </Button>
-      </div>
+        </Row>
+        <Row className="d-flex justify-content-center">
+        <ResumeImage />
+        </Row>
+        </Col>
+      </Container>
+ 
+ 
+
     );
+  
   }
 }
 
