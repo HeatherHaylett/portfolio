beea46f18d1cbb08d2b782f9c5160397f560e5a5
Heather Haylett <heatherhaylett@gmail.com>
2020-07-10 17:50:15 -0500

    just have image and accordian

--- src/components/Exhibit.js
+++ src/components/Exhibit.js
@@ -4,41 +4,41 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import Figure from 'react-bootstrap/Figure';
+import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
 
 function Exhibit(props) {
-    return(
-        <Container >
-          <Row className='align-items-center'>
-            <Col md={3} >
-            <Figure>
-              <Figure.Image
-                width={100}
-                height={'auto'}
-                align={'center'}
-                alt={props.item.title}
-                src={props.item.imgSrc}
-              />
-              <Accordion defaultActiveKey="0">
-                <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                  <Figure.Caption>
-                    {props.item.subTitle}
-                  </Figure.Caption>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="1">
-                  <p>{props.item.description}</p>
-                </Accordion.Collapse>
-              </Accordion>
-            </Figure>
-            </Col>
-            <Col md={9}>
-              <Image className='src' src={props.item.screenShot} alt={props.item.title} fluid/>
-            </Col>
-          </Row>
-        </Container>
-    );
+  return(
+      <Container >
+        <Row className='align-items-start m-30'>
+          <Col md={3} >
+            <Image
+              width={75}
+              height={'auto'}
+              align={'center'}
+              alt={props.item.title}
+              src={props.item.imgSrc}
+              className="d-inline-flex icons"
+              rounded
+              fluid
+            />
+            <Accordion defaultActiveKey="0">
+              <Accordion.Toggle as={Button} eventKey="1">
+                  {props.item.subTitle}
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <p>{props.item.description}</p>
+              </Accordion.Collapse>
+            </Accordion>
+
+          </Col>
+          <Col md={9}>
+            <Image className='src' src={props.item.screenShot} alt={props.item.title} fluid/>
+          </Col>
+        </Row>
+      </Container>
+  );
 }
 
 export default Exhibit;
