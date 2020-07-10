4327a58ca1958ca978a0250e32f439296d07b609
Heather Haylett <heatherhaylett@gmail.com>
2020-07-10 12:46:20 -0500

    add accordian to app descriptions

--- src/components/Exhibit.js
+++ src/components/Exhibit.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Figure from 'react-bootstrap/Figure';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+
 function Exhibit(props) {
     return(
-        <div className="d-inline-block g-card" >
-            <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
-        </div>
+        <Container >
+          <Row className='align-items-center'>
+            <Col md={3} >
+            <Figure>
+              <Figure.Image
+                width={100}
+                height={'auto'}
+                alt="171x180"
+                src={props.item.imgSrc}
+              />
+              <Accordion defaultActiveKey="0">
+                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                  <Figure.Caption>
+                    {props.item.subTitle}
+                  </Figure.Caption>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                  <p>Hello! I'm the body</p>
+                </Accordion.Collapse>
+              </Accordion>
+            </Figure>
+            </Col>
+            <Col md={9}>
+              <Image className='src' src={props.item.screenShot} alt={props.item.title} fluid/>
+            </Col>
+          </Row>
+        </Container>
     );
 }
 
