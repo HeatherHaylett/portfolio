9352191bc39750a49265772e325665855196eb0b
Heather Haylett <heatherhaylett@gmail.com>
2020-07-13 18:27:28 -0500

    make responsive for mobile

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -14,10 +14,10 @@ function HomePage(props) {
   return (
     <Container>
       <Row>
-        <Col className="d-flex justify-content-center">
+        <Col className="d-flex justify-content-center" md={8}>
           <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
         </Col>
-        <Col className="d-flex justify-content-center"><MainImage /></Col>
+        <Col className="d-flex justify-content-center" md={8}>< MainImage /></Col>
       </Row>
     </Container>
   )
